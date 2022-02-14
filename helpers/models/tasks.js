585ec4c429a9b09836c3cd429c4c0415c43b2e00
Gigiox <guillermo.casanova.b@gmail.com>
2022-02-13 23:01:09 -0300

    Added task and tasks classes

---
+++ helpers/models/tasks.js
@@ -0,0 +1,19 @@
+
+
+/**
+ * _list:
+ *      { 'uuid-2348234-238424-3: { id:12, description: asd, completedIn: 20220202 } }
+ */
+
+class Tasks {
+
+    _list = {};
+
+    constructor() {
+        this._list = {};
+    }
+
+
+}
+
+module.exports = Tasks;
